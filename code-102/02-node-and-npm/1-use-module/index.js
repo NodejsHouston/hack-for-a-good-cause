bf2c18f8f68b656152a26f1f2b49e59aabd19057
Saul Maddox <saul.maddox@gmail.com>
2016-05-07 14:05:00 -0500

    first commit

--- code-102/02-node-and-npm/1-use-module/index.js
+++ code-102/02-node-and-npm/1-use-module/index.js
@@ -1,4 +1,4 @@
-const _ = require("lodash");
+const lodash = require("lodash");
 
-console.log(_.isString("Hack for a good cause!"));
+console.log(lodash.isString("Hack for a good cause!"));
 // true
